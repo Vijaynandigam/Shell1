a03b89d906044f9065bcf248b284d5d2947f4dc0
vijay <nandigamvijay2606@gmail.com>
2024-11-09 11:06:47 +1100

    test

--- 01-files.sh
+++ 01-files.sh
@@ -2,12 +2,11 @@
 
 if [ -z "$1" ]; 
 then 
-  echo "Usage: $0 <directory_path>
+  echo "Usage: $0 <directory_path>"
 
 directory=$1
 
 file_count=$(find "$directory" -type f |wc -l)
 
-echo "Number of files in '$directory': $filecount"
-echo" completed" 
+echo "Number of files in '$directory': $file_count"
 
